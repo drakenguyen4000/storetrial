f000029fe67db6042efc6e5c6acf733cefd5e9e5
Drake Nguyen <drakeknguyen@yahoo.com>
2021-04-18 14:36:00 -0700

    clean code Message.js

--- client/src/components/auth/Message.js
+++ client/src/components/auth/Message.js
@@ -1,35 +1,11 @@
-// import { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { Alert } from "reactstrap";
 import { clearMessage } from "../../action/authActions";
-// import { clearErrors } from "../../action/errorActions";
 
 const Message = (props) => {
-  // const [state, setState] = useState({
-  //   display: null,
-  // });
-
-  // console.log(props)
-
-  // useEffect(() => {
-  //   const { error, message, clearMessage, clearErrors } = props;
-  //   //Check for register error
-  //   if (error.id === "LOGIN_FAIL") {
-  //     setState({ display: error.msg.msg });
-  //     setTimeout(() => {
-  //       clearErrors();
-  //     }, 3000);
-  //   } else {
-  //     setState({ display: message });
-  //     setTimeout(() => {
-  //       clearMessage();
-  //     }, 3000);
-  //   }
-  // }, [props]);
   const { message, clearMessage } = props;
 
   const getStyle = (message) => {
-    // const { error, message, clearMessage, clearErrors } = props;
     let msgFlash = null;
     if (message === "Payment success!") {
       msgFlash = "success";
@@ -50,9 +26,7 @@ const Message = (props) => {
 };
 
 const mapStateToProps = (state) => {
-  // console.log("message:", state);
   return {
-    // error: state.error,
     message: state.msg.msg,
   };
 };
